fad3bc21194108dba75ba65a14150a117e721ad3
Denkkou <JoeSchoDev@gmail.com>
2026-05-14 11:30:11 +0300

    Implemented basic readiness endpoint, file server now registered to /app/

--- main.go
+++ main.go
@@ -5,15 +5,19 @@ import (
 	"net/http"
 )
 
-const root = "/"
-const port = "8080"
-
 func main() {
+	const rootPath = "."
+	const appPath = "/app/"
+	const port = "8080"
+
 	// Create new serveMux
 	mux := http.NewServeMux()
 
-	// Standard FileServer as handler for root path
-	mux.Handle(root, http.FileServer(http.Dir(".")))
+	// Standard FileServer as handler
+	mux.Handle(appPath, http.StripPrefix(appPath, http.FileServer(http.Dir(rootPath))))
+
+	// Register readiness endpoint
+	mux.HandleFunc("/healthz", readinessEndpoint)
 
 	// Set server port and handler
 	srv := &http.Server{
@@ -22,6 +26,17 @@ func main() {
 	}
 
 	// Start server
-	log.Printf("Serving files from %s on port: %s\n", root, port)
+	log.Printf("Serving files from %s on port: %s\n", appPath, port)
 	log.Fatal(srv.ListenAndServe()) // Main blocks until server shuts down
+}
+
+func readinessEndpoint(w http.ResponseWriter, req *http.Request) {
+	// Write header
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+
+	// Write status code
+	w.WriteHeader(http.StatusOK)
+
+	// Write body
+	w.Write([]byte(http.StatusText(http.StatusOK)))
 }
